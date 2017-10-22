03d22b97c4c39cdea8c6a71e3832e1b561384290
Ofer <oferstemp@gmail.com>
2017-10-22 08:57:49 +0300

    40

---
+++ src/containers/Book-list.js
@@ -0,0 +1,33 @@
+/**
+ * Created by ofer on 18/10/17.
+ */
+import React, {Component} from 'react';
+import {connect} from "react-redux";
+
+ class BookList extends  Component{
+
+    renderList(){
+        return this.props.books.map((book) => {
+            return (
+                <li key={book.title} className="list-group-item">{book.title}</li>
+            );
+        });
+    }
+
+    render(){
+        return (
+            <ul className="list-group col-sm-4">
+                {this.renderList()}
+            </ul>
+        )
+    }
+}
+
+function mapStateToProps (state){
+     //whatever is returned from here will show up as props inside booklist.
+    return {
+        books: state.books
+    };
+}
+
+export default connect (mapStateToProps)(BookList);
